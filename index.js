e3494d904117e73e5163e8a0e18176e7c98363e1
Jesse Kernaghan <jesse@versett.com>
2017-05-18 17:11:41 -0600

    package is ready to roll and current website info is in

---
+++ index.js
@@ -0,0 +1,74 @@
+export.ContactInfo = {
+    phone: '18448377388',
+    email: 'hi@versett.com',
+    website: 'http://versett.com/'
+};
+
+export.Social = {
+    twitter: 'https://twitter.com/versettinc/',
+    instagram: 'https://www.instagram.com/versettinc/',
+    dribble: 'https://dribbble.com/versett',
+    medium: 'https://medium.com/curated-by-versett'
+};
+
+exports.Locations = [
+    {
+        name: 'Calgary',
+        code: 'YYC',
+        address: '#210 739 11 Ave SW',
+        city: 'Calgary',
+        region: 'AB',
+        postalCode: 'T2R 0E3',
+        country: 'Canada',
+        googleMaps: 'https://www.google.ca/maps/place/739+11+Ave+SW+%23210,+Calgary,+AB+T2R+0E3'
+    },
+    {
+        name: 'New York',
+        code: 'NYC',
+        address: '140 West Street',
+        city: 'New York',
+        region: 'NY',
+        postalcode: '10007',
+        country: 'USA',
+        googleMaps: 'https://www.google.ca/maps/place/New+York+Telephone+Company+Building'
+    }
+];
+
+
+exports.People = [
+    {
+        name: 'Sir Lawrence Douglas Versett',
+        title: 'Founding Dad Figure',
+        website: 'https://en.wikipedia.org/wiki/Lawrence_Douglas_Versett',
+        twitter: 'versettinc'
+    }
+];
+
+
+exports.News = [
+    {
+        title: 'We Helped Emma Roberts launch her book club',
+        description: '',
+        url: 'https://belletrist.com/'
+    },
+    {
+        title: 'Adam writes about building maintainable Angular 2 apps',
+        description: '',
+        url: 'https://medium.com/curated-by-versett/building-maintainable-angular-2-applications-5b9ec4b463a1'
+    },
+    {
+        title: 'Mhhhh. We\'re designing a pizza delivery app for iOS',
+        description: '',
+        url: 'https://www.instagram.com/reaperpie/'
+    },
+    {
+        title: 'We’ve got feet on the ground in Paris working with online booking tool KDS',
+        description: '',
+        url: 'http://www.kds.com/'
+    },
+    {
+        title: 'Why we refreshed our brand',
+        description: '',
+        url: 'https://medium.com/curated-by-versett/rebranding-versett-49ebf4d1bbb6#.a0n92o2qb'
+    }
+];
